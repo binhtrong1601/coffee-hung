8d26bd2da8f3e5f1c4c2d27aac09cf9a8d481bc3
binh-trong <binhtrong1601@gmail.com>
2023-06-14 15:36:27 +0700

    â

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { DownOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Dropdown, message, Space } from "antd";
+const onClick = ({ key }) => {
+  message.info(`Click on item ${key}`);
+};
+const items = [
+  {
+    label: "Tất cả",
+    key: "1",
+  },
+  {
+    label: "Cà phê Việt Nam",
+    key: "2",
+  },
+  {
+    label: "Tại nhà",
+    key: "3",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo">
+          <img src="https://noreruns.net/wp-content/uploads/2017/02/Passengers-Logo.jpg" />
+        </div>
+        <div className="dropdown">
+          <Dropdown
+            menu={{
+              onClick,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                Cà phê
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+          <Dropdown
+            menu={{
+              items,
+              onClick,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                Menu
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+          <Dropdown
+            menu={{
+              onClick,
+            }}
+          >
+            <a onClick={(e) => e.preventDefault()}>
+              <Space>
+                Cửa Hàng
+                <DownOutlined />
+              </Space>
+            </a>
+          </Dropdown>
+        </div>
+        <div className="card">
+          <ShoppingCartOutlined />
+        </div>
       </header>
     </div>
   );
